cca7ed8ab3a4bf0b11a51bf3457d37b0a1e6a946
MahyarCode <mahyar.mousavi.1408@gmail.com>
2026-04-08 16:37:35 +0400

    implemented PeopleReviews functionality to see other reviews

--- src/components/Reviews/PeopleReviews.tsx
+++ src/components/Reviews/PeopleReviews.tsx
@@ -1,46 +1,90 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import { reviewsData } from "../../data/reviewsData";
 
 const PeopleReviews: React.FC = () => {
+  const [reviewIndex, setReviewIndex] = useState<number>(0);
+
+  const handleReviewNavigation = useCallback(
+    (navigate: string) => {
+      if (navigate === "next") {
+        if (reviewIndex === reviewsData.length - 1) {
+          setReviewIndex(0);
+        } else {
+          setReviewIndex((prev) => prev + 1);
+        }
+      } else if (navigate === "previous") {
+        if (reviewIndex === 0) {
+          setReviewIndex(reviewsData.length - 1);
+        } else {
+          setReviewIndex((prev) => prev - 1);
+        }
+      }
+    },
+    [reviewIndex],
+  );
+
+  const handleReviewCircleButtonClick = useCallback((indexNumber: number) => {
+    setReviewIndex(indexNumber);
+  }, []);
+
   return (
     <>
       <div className="grid grid-cols-[15%_70%_15%] font-averia">
         <div className="grid col-start-2 bg-green-200 rounded-2xl">
           <div className="flex">
             <div>
-              <img src="./user-1.jpg" className="w-30 py-4 pl-4 rounded-full" />
+              <img
+                src={reviewsData[reviewIndex].profile.img}
+                className="w-30 py-4 pl-4 rounded-full"
+              />
             </div>
             <div className="flex flex-col ">
               <div>
-                <p className="pl-8 pt-8 text-2xl">Mahyar MousvaiNia</p>
+                <p className="pl-8 pt-8 text-2xl">
+                  {reviewsData[reviewIndex].profile.name}
+                </p>
               </div>
               <div>
-                <p className="pl-8 text-xl">Yerevan, Armenia</p>
+                <p className="pl-8 text-xl">
+                  {reviewsData[reviewIndex].profile.location}
+                </p>
               </div>
             </div>
           </div>
           <div className="grid grid-cols-[15%_70%_15%] justify-self-center h-75 mt-8 ">
             <div className="grid justify-center items-center h-40">
-              <button className="text-2xl">⬅️</button>
+              <button
+                onClick={() => handleReviewNavigation("previous")}
+                className="text-2xl"
+              >
+                ⬅️
+              </button>
             </div>
             <div className="grid justify-self-center">
               <p className="text-2xl text-justify">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Accusantium quas quisquam non? Quas voluptate nulla minima
-                deleniti optio ullam nesciunt, numquam corporis et asperiores
-                laboriosam sunt, praesentium suscipit blanditiis. Necessitatibus
-                id alias reiciendis, perferendis facere pariatur dolore veniam
-                autem esse non voluptatem saepe provident nihil molestiae.
+                {reviewsData[reviewIndex].text}
               </p>
             </div>
             <div className="grid justify-center items-center h-40">
-              <button className="text-2xl">➡️</button>
+              <button
+                onClick={() => handleReviewNavigation("next")}
+                className="text-2xl"
+              >
+                ➡️
+              </button>
             </div>
           </div>
           <div className="flex justify-center pb-4">
             <div className="flex gap-3">
-              <button className="bg-gray-600 w-3 h-3 rounded-full"></button>
-              <button className="bg-gray-400 w-3 h-3 rounded-full"></button>
-              <button className="bg-gray-400 w-3 h-3 rounded-full"></button>
+              {reviewsData.map((btn, index) => {
+                return (
+                  <button
+                    key={index}
+                    className={`${index === reviewIndex ? "bg-gray-600" : "bg-gray-400"} w-3 h-3 rounded-full hover:cursor-pointer p-2`}
+                    onClick={() => handleReviewCircleButtonClick(index)}
+                  ></button>
+                );
+              })}
             </div>
           </div>
         </div>
